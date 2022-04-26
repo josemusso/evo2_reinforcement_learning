f3ea8f6ff557a7287a1ff6096fe1098faf52a82b
practicaventures <practicaventures@digevo.com>
2022-04-26 16:17:03 -0400

    render.py con las funciones de render

--- app/environments/evolution/evolution/envs/evolution.py
+++ app/environments/evolution/evolution/envs/evolution.py
@@ -12,123 +12,15 @@ import config
 from stable_baselines import logger
 import pandas as pd
 from tabulate import tabulate
-
-os.environ["DISPLAY"] = ":0"
-
 import pygame, sys
 from pygame.locals import *
 import time
 import pygame
-
 import random
 
-def grid(window, size, rows, cols):
+os.environ["DISPLAY"] = ":0"
 
-    offset = size #offset grilla
-
-    total_y = size * rows #tamano total eje y
-    total_x = size * cols #tamano total eje x
-    # distanceBtwRows = size // rows  esto es size ahora
-
-    x = offset
-    y = offset
-
-    print('dibujando lineas horizontales')
-    cont_cols = 0
-    cont_rows = 0
-    for l in range(rows+1):
-        print('x1', 'y1',x, y)
-        print('x2', 'y2',total_x, y)
-
-
-        pygame.draw.line(window, (0,0,0), (x,y), (offset + total_x, y))
-        y += size
-        cont_cols+=1
-
-    x = offset
-    y = offset
-
-    print('dibujando lineas verticales')
-
-    for z in range(cols+1):
-
-        print('x1', 'y1',x, y)
-        print('x2', 'y2',x, total_y)
-
-        pygame.draw.line(window, (0,0,0), (x,y), (x, offset + total_y))
-        x += size
-        cont_rows+=1
-
-
-
-def circle(window, size, var_1, var_2):
-
-
-    circle_x = size + var_2*size + (size/2)
-    circle_y = size + var_1*size + (size/2)
-    pygame.draw.circle(window, (0, 255, 0),
-                   [circle_x, circle_y], 10, 2)
-
-def label (window, size, rows_name, cols_name):
-
-    myfont = pygame.font.SysFont("monospace", 15)
-
-    # render text
-    label = myfont.render(cols_name, 1, (0,0,0))
-    label2 = myfont.render(rows_name, 2, (0,0,0))
-    window.blit(label, (size, size/4))
-    window.blit(label2, (0, size))
-
-def options(window, size, options_1, options_2):
-
-
-    myfont =  pygame.font.SysFont("monospace", 10)
-    x = size
-    y = size/2
-    for i, value in enumerate(options_2):
-        label = myfont.render(value, 1, (0,0,0))
-        window.blit(label, (x*(i+1) + size/2, y))
-
-    x = size/2
-    y = size
-
-    for i, value in enumerate(options_1):
-        label = myfont.render(value, 1, (0,0,0))
-        window.blit(label, (x, y*(i+1) + size/2))
-
-def draw_grid(window, size, row, cols, labels, opt1, opt2, x, y):
-
-    window.fill((255,255,255))
-    grid(window, size, rows, cols)
-    circle(window, size, x, y)
-    label(window, size, labels[0], labels[1])
-    options(window, size, opt1, opt2)
-    pygame.display.update()
-
-
-def game(running,size, rows, cols, x, y):
-    window_width = cols*size + 2*size
-    window_height = rows*size + 2*size 
-    pygame.init()
-
-    self.window = pygame.display.set_mode((window_width, window_height))
-    labels = [self.board1.get_var_1(), self.board1.get_var_2()]
-    opt1 = self.board1.get_options_var_1()
-    opt2 = self.board1.get_options_var_2()
-    draw_grid(self.window, size, rows, cols, labels, opt1, opt2, 0,0)
-
-    while running.value:
-    
-        # Check for event if user has pushed
-        # any event in queue
-        for event in pygame.event.get():
-        
-            # if event is of type quit then set
-            # running bool to false
-            if event.type == pygame.QUIT:
-                print('finalizado')
-                running.value = False
-        draw_grid(self.window, size, rows, cols, labels, opt1, opt2, x.value,y.value)
+from render import grid, circle, label, options, draw_grid, game
 
 '''
 def redraw(window, x, y):
--- app/environments/evolution/evolution/envs/render.py
+++ app/environments/evolution/evolution/envs/render.py
@@ -1,147 +1,110 @@
+def grid(window, size, rows, cols):
 
-from pyglet.gl import *
-from pyglet.window import mouse
-from pyglet.window import key
-from pyglet import shapes
+    offset = size #offset grilla
 
-options_var_1 = 3
-options_var_2 = 4
-offset = 100
-square_size = 100
-window_width = square_size*options_var_1 + 2*offset
-window_height = square_size*options_var_2 + 2*offset
+    total_y = size * rows #tamano total eje y
+    total_x = size * cols #tamano total eje x
+    # distanceBtwRows = size // rows  esto es size ahora
 
+    x = offset
+    y = offset
 
-def draw_grid(group, batch, opt1, opt2):
-
-    #dibujar lineas verticales dearriba hacia abajo
-
-    y_1 = offset
-    y_2 = offset + options_var_2*square_size
-    width = 2
- 
-    # color = green
-    color_list = ([0.3, 0.3, 0.3] * 2)
-    #line = shapes.Line(1, 1, 2, 2, width, color=color, batch=batch) 
-
-    for vertical in range(options_var_1+1):
-        #x1 y x2 es igual, pero va cammbiando
-        x_1 = offset + square_size*vertical
-        x_2 = offset + square_size*vertical
-        #batch.add(shapes.Line(x_1, y_1, x_2, y_2, width, color=color, batch=batch)) 
-        batch.add(2, pyglet.gl.GL_LINES, group,
-              ('v2i/static', (x_1, y_1, x_2, y_2)), ('c3f/static', color_list))
-
-    x_1 = offset
-    x_2 = offset + options_var_1*square_size
-
-    for horizontal in range(options_var_2+1): 
-        y_1 = offset + square_size*horizontal
-        y_2 = offset + square_size*horizontal
-        #line2 = shapes.Line(x_1, y_1, x_2, y_2, width, color=color, batch=batch) 
-        batch.add(2, pyglet.gl.GL_LINES, group,
-              ('v2i/static', (x_1, y_1, x_2, y_2)), ('c3f/static', color_list))
-
-def draw_player(group,batch, var_1, var_2):
+    print('dibujando lineas horizontales')
+    cont_cols = 0
+    cont_rows = 0
+    for l in range(rows+1):
+        print('x1', 'y1',x, y)
+        print('x2', 'y2',total_x, y)
 
 
-    circle_x = offset + var_1*square_size + (square_size/2)
-    circle_y = offset + var_2*square_size + (square_size/2)
+        pygame.draw.line(window, (0,0,0), (x,y), (offset + total_x, y))
+        y += size
+        cont_cols+=1
+
+    x = offset
+    y = offset
+
+    print('dibujando lineas verticales')
+
+    for z in range(cols+1):
+
+        print('x1', 'y1',x, y)
+        print('x2', 'y2',x, total_y)
+
+        pygame.draw.line(window, (0,0,0), (x,y), (x, offset + total_y))
+        x += size
+        cont_rows+=1
+
+
+
+def circle(window, size, var_1, var_2):
+
+
+    circle_x = size + var_2*size + (size/2)
+    circle_y = size + var_1*size + (size/2)
+    pygame.draw.circle(window, (0, 255, 0),
+                   [circle_x, circle_y], 10, 2)
+
+def label (window, size, rows_name, cols_name):
+
+    myfont = pygame.font.SysFont("monospace", 15)
+
+    # render text
+    label = myfont.render(cols_name, 1, (0,0,0))
+    label2 = myfont.render(rows_name, 2, (0,0,0))
+    window.blit(label, (size, size/4))
+    window.blit(label2, (0, size))
+
+def options(window, size, options_1, options_2):
+
+
+    myfont =  pygame.font.SysFont("monospace", 10)
+    x = size
+    y = size/2
+    for i, value in enumerate(options_2):
+        label = myfont.render(value, 1, (0,0,0))
+        window.blit(label, (x*(i+1) + size/2, y))
+
+    x = size/2
+    y = size
+
+    for i, value in enumerate(options_1):
+        label = myfont.render(value, 1, (0,0,0))
+        window.blit(label, (x, y*(i+1) + size/2))
+
+def draw_grid(window, size, row, cols, labels, opt1, opt2, x, y):
+
+    window.fill((255,255,255))
+    grid(window, size, rows, cols)
+    circle(window, size, x, y)
+    label(window, size, labels[0], labels[1])
+    options(window, size, opt1, opt2)
+    pygame.display.update()
+
+
+def game(running,size, rows, cols, x, y):
+    window_width = cols*size + 2*size
+    window_height = rows*size + 2*size 
+    pygame.init()
+
+    self.window = pygame.display.set_mode((window_width, window_height))
+    labels = [self.board1.get_var_1(), self.board1.get_var_2()]
+    opt1 = self.board1.get_options_var_1()
+    opt2 = self.board1.get_options_var_2()
+    draw_grid(self.window, size, rows, cols, labels, opt1, opt2, 0,0)
+
+    while running.value:
     
-    # size of circle
-    # color = green
-    size_circle = (square_size/2) - 30
-    
-    # creating a circle
-    circle1 = shapes.Circle(circle_x, circle_y, size_circle, color =(0, 0, 0), batch = batch, group=group)
-    
-    # changing opacity of the circle1
-    # opacity is visibility (0 = invisible, 255 means visible)
-    circle1.opacity = 250
-    circle1.circle_x = offset + 2*square_size + (square_size/2)
-
-    circle1.draw()
-
-
-
-
-
-
-
-
-
-
-def draw_axis_names(group,batch, var_name_1, var_name_2):
-
-    label = pyglet.text.Label(var_name_1,
-                          font_name='Times New Roman',
-                          font_size=12,
-                          x=window_width//2, y=window_height-100,
-                          anchor_x='center', anchor_y='center',
-                          color=(0, 0, 0, 0), batch=batch, group=group)
-
-    label1 = pyglet.text.Label(var_name_2,
-                          font_name='Times New Roman',
-                          font_size=12,
-                          x=window_width-100, y=window_height//2,
-                          anchor_x='center', anchor_y='center',
-                          color=(255, 255, 255, 255), batch=batch, group=group)
-
-    label.draw()
-
-
-
-
-def draw_var_opt(var_opt_1, var_opt_2):
-    #Debe dibujar los valores para cada opcion de la grilla
-
-    return
-
-class MainWindow(pyglet.window.Window):
-    def __init__(self, *args, **kwargs):
-        super().__init__(*args, **kwargs) #inicializa una clase superior window para acceder a sus metodos
-        #self.set_minimum_size(400,300)
-
-    def on_draw(self):
-        #screen = pyglet.canvas.get_display().get_default_screen()
-        #window_width = int(min(screen.width, screen.height) * 2 / 3)
-
-        pyglet.gl.glClearColor(255, 255, 255, 255)
-        window.clear()
-
-        #pyglet.gl.glLineWidth(3)
-        batch = pyglet.graphics.Batch()
-        grid = pyglet.graphics.OrderedGroup(0)
-        labels = pyglet.graphics.OrderedGroup(1)
-        player = pyglet.graphics.OrderedGroup(2)
-
-        # draw the grid and labels
-
-        draw_grid(grid,batch, options_var_1, options_var_2)
-        draw_axis_names(labels, batch,'cant_fundadores', 'horas_dedicacion')
-        draw_player(player, batch, 2, 2)
-
-        batch.draw()
-
-        #self.triangle.vertices.draw(GL_TRIANGLE_FAN)
-
-if __name__== "__main__":
-
-                
-
-    window = MainWindow(window_width, window_height, "PygetTutorials")
-    label = pyglet.text.Label('Hola',
-                          font_name ='Times New Roman',
-                          font_size = 28,
-                          x = 20, y = window.height//2,
-                            color=(0,0,0,0) )
-    window.on_draw()
-    window.clear()
-    label.draw()
-
-    #window2 = MainWindow(300, 200, "hola")
-
-    pyglet.app.run()
+        # Check for event if user has pushed
+        # any event in queue
+        for event in pygame.event.get():
+        
+            # if event is of type quit then set
+            # running bool to false
+            if event.type == pygame.QUIT:
+                print('finalizado')
+                running.value = False
+        draw_grid(self.window, size, rows, cols, labels, opt1, opt2, x.value,y.value)
 
 '''
 
